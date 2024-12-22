52fc9c2b2888857f35c27a9942d1e93f35a7fa8c
SameeraBhanu <183719089+sameerabhanu@users.noreply.github.com>
2024-12-22 15:47:05 +0530

    completed filterLongerWords

--- filter.js
+++ filter.js
@@ -1,13 +1,22 @@
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
+
 const isEven = function (number) {
   return (number & 1) === 0;
 };
+
 const filterEvenNumbers = function (numbers) {
   return numbers.filter(isEven);
 };
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
-const filterLongWords = function (words) { };
+
+const hasMoreThan5Chars = function (string) {
+  return string.length > 5;
+};
+
+const filterLongWords = function (words) {
+  return words.filter(hasMoreThan5Chars);
+};
 
 // people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
 const filterAdults = function (people) { };
